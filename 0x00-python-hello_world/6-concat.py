591d72432e64e5d194d3979a2a8d3fdd7ab009e5
Reginald-kyalo <reginald.kyalo@gmail.com>
2023-05-09 03:27:18 -0700

    added python file

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-# YOUR CODEEASE REMOVE THIS LINE
-print(f"Welcome to {str1} {str2}!")
+str1 = str1+str2
+print(f"Welcome to {str1[:10]} {str1[10:]}!")
