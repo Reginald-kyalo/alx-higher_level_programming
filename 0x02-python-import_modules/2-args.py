29814beda4620c146699a67ba09f24257530eae7
Reginald-kyalo <reginald.kyalo@gmail.com>
2023-05-12 12:41:53 -0700

    added python file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 import sys
-
-count = len(sys.argv) - 1
+if __name__ == "__main__":
+    count = len(sys.argv) - 1
 if count == 0:
     print("0 arguments.")
 elif count == 1:
@@ -10,4 +10,4 @@ elif count == 1:
 else:
     print("{} arguments:".format(count))
 for i in range(count):
-     print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    print("{}: {}".format(i + 1, sys.argv[i + 1]))
