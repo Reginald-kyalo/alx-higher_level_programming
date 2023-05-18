9b254700e7026f0b07d3bd7a15e829e1e742cd4e
Reginald-kyalo <reginald.kyalo@gmail.com>
2023-05-18 11:59:34 -0700

    added python file

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    max_key = None
-    max_value = float(-inf)
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
 
-    for key, value in a_dictionary.items():
-        if is isinstance(value,int) and value > max_value:
-            max_key = key
-            max_value = value
-    return max_key
+    ret = list(a_dictionary.keys())[0]
+    big = a_dictionary[ret]
+    for k, v in a_dictionary.items():
+        if v > big:
+            big = v
+            ret = k
+    return (ret)
