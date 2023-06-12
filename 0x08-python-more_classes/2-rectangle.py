f75b25d71e85aa6dbacd410611e7b54773f37886
Reginald-kyalo <reginald.kyalo@gmail.com>
2023-06-12 09:42:25 -0700

    added a pyhton file

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#1/usr/bin/python3
 """
 defines a class Rectangle
 """
@@ -37,3 +37,13 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    def area(self):
+        """returns area of rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """returns perimeter of rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width * 2) + (self.__height * 2)
