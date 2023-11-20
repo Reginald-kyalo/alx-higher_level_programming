0903ad84e75eb05eb02a6df81dcd807468b26484
Reginald-kyalo <reginald.kyalo@gmail.com>
2023-11-20 08:03:24 -0800

    added some changes

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-#lists cities and state name
+"""
+lists cities and state name
+from database passed as argument
+"""
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    """establish connection to database"""
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor();
     cursor.execute("SELECT `cities`.`id`, `cities`.`name`, `states`.`name` \
                     FROM `cities` \
                     INNER JOIN `states` \
-                        ON `cities`.`state`.`id` \
+                        ON `cities`.`id` \
                     ORDER BY `cities`.`id` ASC")
     states = cursor.fetchall()
 
@@ -18,4 +20,4 @@ if __name__ == "__main__":
         print(state)
 
     cursor.close()
-    connection.close
+    db.close
