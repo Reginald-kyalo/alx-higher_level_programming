da400ccfab26284c2059420847bf9ea73761e1cf
Reginald-kyalo <reginald.kyalo@gmail.com>
2023-11-20 04:28:10 -0800

    added some changes

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-#lists state with name Arizona passed as argument
+"""
+Lists state with name passed as argument.
+"""
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    """establish connection to database"""
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cursor = db.cursor();
-    cursor.execute("SELECT * FROM states WHERE BINARY `name` = '{}' ORDER BY states.id ASC".format(sys.argv[4])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states\
+                    WHERE BINARY `name` = '{}'\
+                    ORDER BY states.id ASC".format(sys.argv[4]))
     states = cursor.fetchall()
 
     for state in states:
         print(state)
 
     cursor.close()
-    connection.close
+    db.close
