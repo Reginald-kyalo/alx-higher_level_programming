588be644445b7b20ea76f1c45258ee7626e0ec81
Reginald-kyalo <reginald.kyalo@gmail.com>
2023-11-20 08:13:45 -0800

    added some changes

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     cursor.execute("SELECT `cities`.`id`, `cities`.`name`, `states`.`name` \
                     FROM `cities` \
                     JOIN `states` \
-                        ON `cities`.`state_id` \
+                        ON `states`.`id` = `cities`.`state_id` \
                     ORDER BY `cities`.`id` ASC")
     states = cursor.fetchall()
 
