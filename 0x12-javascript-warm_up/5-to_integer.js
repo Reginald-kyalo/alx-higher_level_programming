29221071663fecf21bf6e9c22bafe47cb07224a1
reginaldKyalo <111074460+Reginald-kyalo@users.noreply.github.com>
2024-04-23 20:16:41 +0300

    Update 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,10 +3,10 @@
 if (typeof process.argv[2] === 'string') {
   const num = parseInt(process.argv[2], 10);
   if (isNaN(num)) {
-	  console.log('Not a number');
-	} else {
-		console.log('My number:', num);
-	}
+    console.log('Not a number');
+  } else {
+    console.log('My number:', num);
+}
 } else if (typeof process.argv[2] === 'number') {
   console.log(Math.floor(process.argv[2]));
 } else {
