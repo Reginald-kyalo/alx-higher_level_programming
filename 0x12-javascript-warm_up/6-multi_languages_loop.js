a56fed44258c1b38d0ba8fa068edf48d74b092b9
reginaldKyalo <111074460+Reginald-kyalo@users.noreply.github.com>
2024-04-23 20:19:12 +0300

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-let array = ["C is fun", "Python is cool", "Javascript is amazing"]
 
-for (let element of array)
-{
-	console.log(element);
+let array = ['C is fun', 'Python is cool', 'Javascript is amazing']
+
+for (let element of array) {
+  console.log(element);
 }
