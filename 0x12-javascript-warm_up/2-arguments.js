2f1d87e5e825ef275602965ab4acc49e40a8a5b1
reginaldKyalo <111074460+Reginald-kyalo@users.noreply.github.com>
2024-04-23 19:55:41 +0300

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,15 +2,10 @@
 
 let i = process.argv.length - 2;
 
-if (i > 1)
-{
-    console.log("Arguments found");
-}
-else if (i === 1)
-{
-    console.log("Argument found");
-}
-else
-{
-    console.log("No argument");
+if (i > 1) {
+  console.log("Arguments found");
+} else if (i === 1) {
+  console.log("Argument found");
+} else {
+  console.log("No argument");
 }
