46b50a3260fde6559bf1c3e03c9b4f6b63cfaf7e
reginaldKyalo <111074460+Reginald-kyalo@users.noreply.github.com>
2024-04-23 20:24:20 +0300

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
 
 let num = parseInt(process.argv[2], 10);
-let str = "C is fun";
-let i = 0;
+const str = 'C is fun';
+const i = 0;
 
-if (isNaN(num))
-{
-    console.log("Missing number of occurrences");
+if (isNaN(num)) {
+  console.log('Missing number of occurrences');
 }
-while(num > i)
-{
-    console.log(str);
-    num--;
+
+while (num > i) {
+  console.log(str);
+  num--;
 }
