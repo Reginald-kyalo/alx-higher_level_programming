156d70519104089d158dd423cb51ff4874f47c60
reginaldKyalo <111074460+Reginald-kyalo@users.noreply.github.com>
2024-04-23 20:32:50 +0300

    Update 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 
-let num = parseInt(process.argv[2], 10);
-let num2 = parseInt(process.argv[3], 10);
+const num = parseInt(process.argv[2], 10);
+const num2 = parseInt(process.argv[3], 10);
 
-function add(a, b)
-{
-	console.log(a + b);
+function add(a, b) {
+  console.log(a + b);
 }
 
 add(num, num2);
