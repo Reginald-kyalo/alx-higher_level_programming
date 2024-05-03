79bf0864e2c21f0b683e7099f01525afa89c549a
reginaldKyalo <111074460+Reginald-kyalo@users.noreply.github.com>
2024-05-04 00:23:55 +0300

    Update 14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -6,7 +6,7 @@ from model_state import Base, State
 from model_city import City
 import sys
 
-if __name__ == "main":
+if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1],
                                    sys.argv[2],
