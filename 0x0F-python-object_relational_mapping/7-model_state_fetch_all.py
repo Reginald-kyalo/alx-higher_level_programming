76ccbfd3b4acebe146ba957df6dfc14d33360b80
reginaldKyalo <111074460+Reginald-kyalo@users.noreply.github.com>
2024-05-03 23:18:34 +0300

    Update 7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-# list all State objects from the database hbtn_0e_6_usa
+"""list all State objects from the database hbtn_0e_6_usa
+"""
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import State
