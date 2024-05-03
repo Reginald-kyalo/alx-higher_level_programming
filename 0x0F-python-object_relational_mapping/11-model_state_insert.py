a5f66531e8fff4e33f2ce01f86d98d40eb2cd0ac
reginaldKyalo <111074460+Reginald-kyalo@users.noreply.github.com>
2024-05-03 23:35:00 +0300

    Update 11-model_state_insert.py

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python3
+"""adds the State object “Louisiana” to the database hbtn_0e_6_usa
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 import sys
 
