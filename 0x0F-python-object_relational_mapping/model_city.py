fdd6fde3c80994b5fd6e39eb95f039f242a80fd6
reginaldKyalo <111074460+Reginald-kyalo@users.noreply.github.com>
2024-05-03 23:27:37 +0300

    Update model_city.py

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -3,7 +3,7 @@ from model_state import Base, State
 
 """Defines class State"""
 class City(Base):
-    __tablename__ - 'cities'
+    __tablename__ = 'cities'
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
