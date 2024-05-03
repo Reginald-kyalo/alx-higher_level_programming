935635bffa213f851f1c3f0f1795cd07d4bec9f0
Reginald-kyalo <reginald.kyalo@gmail.com>
2024-05-03 14:18:12 -0700

    added changes

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     session = Session()
 
     for state in session.query(State).order_by(State.id):
-		if "a" in state.name:
-			print("{}: {}".format(state.id, state.name))
+        if "a" in state.name:
+            print("{}: {}".format(state.id, state.name))
 
     session.close()
